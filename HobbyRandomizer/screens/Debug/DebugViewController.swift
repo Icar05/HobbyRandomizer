6d55ae80ffa3f09717db1896c5735ea22659d276
Aleksandr Galiatkin <ohaliatkin@competo.io>
2022-06-08 20:32:29 +0300

    remove unused

---
+++ HobbyRandomizer/screens/Debug/DebugViewController.swift
@@ -0,0 +1,70 @@
+//
+//  DebugViewController.swift
+//  HobbyRandomizer
+//
+//  Created by ICoon on 08.06.2022.
+//
+
+import UIKit
+
+class DebugViewController: UIViewController {
+    
+    
+    
+    
+    fileprivate var count = 7
+    
+    @IBOutlet weak var countLabel: BangButton!
+    
+    @IBOutlet weak var randomizer: RandomizerUtil!
+    
+    @IBOutlet weak var incrementLabel: BangButton!
+    
+    @IBOutlet weak var decrementLabel: BangButton!
+    
+    
+    
+
+    @available(iOS, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init() {
+        super.init(nibName: "DebugViewController", bundle: Bundle.main)
+    }
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        self.countLabel.text = "\(count)"
+        
+        
+        incrementLabel.isUserInteractionEnabled = true
+        decrementLabel.isUserInteractionEnabled = true
+        
+        let incGest = UITapGestureRecognizer(target: self, action: #selector(increment(tapGestureRecognizer:)))
+        incrementLabel.addGestureRecognizer(incGest)
+        
+        let decGest = UITapGestureRecognizer(target: self, action: #selector(decrement(tapGestureRecognizer:)))
+        decrementLabel.addGestureRecognizer(decGest)
+    }
+
+    @objc func increment(tapGestureRecognizer: UITapGestureRecognizer){
+        if(count < 100){
+            self.count += 1
+            self.countLabel.text = "\(count)"
+            self.randomizer.setDataSource(count: count)
+        }
+    }
+   
+    
+    @objc func decrement(tapGestureRecognizer: UITapGestureRecognizer){
+        if(count >  1){
+            self.count -= 1
+            self.countLabel.text = "\(count)"
+            self.randomizer.setDataSource(count: count)
+        }
+    }
+    
+}
