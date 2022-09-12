840ebac711c77b9b22fa34e6eed68c677770558d
Alex <ohaliatkin@competo.io>
2022-09-12 10:01:29 +0300

    remove BLE module

--- RS/screens/Test/TestViewController.swift
+++ RS/screens/Test/TestViewController.swift
@@ -6,34 +6,34 @@
 //
 
 import UIKit
-import BLEApi
+//import BLEApi
 
 class TestViewController: UIViewController {
     
     
-    private let colors: [InfoType: UIColor] = [
-        .connection: UIColor.blue,
-        .discovering: UIColor.systemGreen,
-        .extra: UIColor.purple,
-        .error: UIColor.red,
-        .transportOut: UIColor.brown,
-        .transportIn: UIColor.coolOrange!,
-        .current: UIColor.black
-    ]
-    
-    private let client = BLEClientImpl()
-    
-    @IBOutlet weak var scrollView: UIScrollView!
-    
-    @IBOutlet weak var stateSwitch: UISwitch!
-    
-    @IBOutlet weak var stateLabel: UILabel!
-    
-    @IBOutlet weak var debugLabel: UILabel!
-    
-    @IBOutlet weak var errorLabel: UILabel!
-    
-    
+//    private let colors: [InfoType: UIColor] = [
+//        .connection: UIColor.blue,
+//        .discovering: UIColor.systemGreen,
+//        .extra: UIColor.purple,
+//        .error: UIColor.red,
+//        .transportOut: UIColor.brown,
+//        .transportIn: UIColor.coolOrange!,
+//        .current: UIColor.black
+//    ]
+//
+//    private let client = BLEClientImpl()
+//
+//    @IBOutlet weak var scrollView: UIScrollView!
+//
+//    @IBOutlet weak var stateSwitch: UISwitch!
+//
+//    @IBOutlet weak var stateLabel: UILabel!
+//
+//    @IBOutlet weak var debugLabel: UILabel!
+//
+//    @IBOutlet weak var errorLabel: UILabel!
+//
+//
     
     
     @available(iOS, unavailable)
@@ -49,10 +49,10 @@ class TestViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.stateSwitch.addTarget(self, action: #selector(switchChanged), for: UIControl.Event.valueChanged)
-        
-        self.client.delegate = self
-        self.stateLabel.text = "Api Disabled"
+//        self.stateSwitch.addTarget(self, action: #selector(switchChanged), for: UIControl.Event.valueChanged)
+//
+//        self.client.delegate = self
+//        self.stateLabel.text = "Api Disabled"
         
         
 //        self.client.callback = { state in
@@ -93,48 +93,48 @@ class TestViewController: UIViewController {
         
         
         
-        let model = BLEApiData(
-            data: Data(Test.getText().utf8),
-            title: "Chemical brothers remix",
-            info: "String data")
-        
-        self.client.sendData(data: model)
-    }
-    
-    @objc func switchChanged(mySwitch: UISwitch) {
-        self.stateLabel.text = mySwitch.isOn ?  "Stop servise" : "Start Service"
-        mySwitch.isOn ?  client.startBLEApi() : client.stopBLEApi()
-    }
+//        let model = BLEApiData(
+//            data: Data(Test.getText().utf8),
+//            title: "Chemical brothers remix",
+//            info: "String data")
+//
+//        self.client.sendData(data: model)
+//    }
+//
+//    @objc func switchChanged(mySwitch: UISwitch) {
+//        self.stateLabel.text = mySwitch.isOn ?  "Stop servise" : "Start Service"
+//        mySwitch.isOn ?  client.startBLEApi() : client.stopBLEApi()
+//    }
 
 }
 
-extension TestViewController: BLEApiDelegate{
-    
-    
-    func displayInfo(model: InfoData) {
-        if(model.type != .current ){
-            return
-        }
-        
-        self.appendText(
-            newValue: model.info,
-            color: colors[model.type]!
-        )
-    }
-    
-    private func appendText(newValue: String, color: UIColor){
-        let combination = NSMutableAttributedString()
-        let prevText = self.debugLabel.attributedText ?? NSAttributedString.init(string: "")
-        let newText = colorString(value: newValue + "\n", color: color)
-        combination.append(prevText)
-        combination.append(newText)
-        self.debugLabel.attributedText = combination
-    }
-    
-    private func colorString(value: String, color: UIColor) -> NSAttributedString{
-        return NSAttributedString(
-            string: value,
-            attributes: [NSAttributedString.Key.foregroundColor : color])
-    }
-    
+//extension TestViewController: BLEApiDelegate{
+//
+//
+////    func displayInfo(model: InfoData) {
+////        if(model.type != .current ){
+////            return
+////        }
+////
+////        self.appendText(
+////            newValue: model.info,
+////            color: colors[model.type]!
+////        )
+////    }
+////
+////    private func appendText(newValue: String, color: UIColor){
+////        let combination = NSMutableAttributedString()
+////        let prevText = self.debugLabel.attributedText ?? NSAttributedString.init(string: "")
+////        let newText = colorString(value: newValue + "\n", color: color)
+////        combination.append(prevText)
+////        combination.append(newText)
+////        self.debugLabel.attributedText = combination
+////    }
+////
+////    private func colorString(value: String, color: UIColor) -> NSAttributedString{
+////        return NSAttributedString(
+////            string: value,
+////            attributes: [NSAttributedString.Key.foregroundColor : color])
+////    }
+////
 }
