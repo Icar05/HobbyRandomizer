3d7f873e043e0ad71b8e78730d68e317dbb83928
Aleksandr Galiatkin <ohaliatkin@competo.io>
2022-09-13 21:56:21 +0300

    file util

---
+++ RS/appComponents/dependencies/OpenFileUtil.swift
@@ -0,0 +1,86 @@
+//
+//  OpenFileUtil.swift
+//  RS
+//
+//  Created by ICoon on 13.09.2022.
+//
+
+import Foundation
+import UniformTypeIdentifiers
+import MobileCoreServices
+
+protocol OpenFileUtilDelegate: NSObject{
+    func onError(error: Error)
+    func onImportFile(file: Data)
+}
+
+public final class OpenFileUtil: NSObject{
+    
+    
+    
+    private let documentPicker: UIDocumentPickerViewController
+    
+    weak var delegate: OpenFileUtilDelegate?
+    
+    
+    func startPicker(view: UIViewController){
+        documentPicker.delegate = self
+        documentPicker.modalPresentationStyle = .overFullScreen
+        
+        view.present(documentPicker, animated: true)
+    }
+    
+    
+    override init(){
+        if #available(iOS 14, *) {
+            // iOS 14 & later
+            let supportedTypes: [UTType] = [UTType.text, UTType.json, UTType.folder]
+            documentPicker = UIDocumentPickerViewController(forOpeningContentTypes: supportedTypes)
+        } else {
+            // iOS 13 or older code
+            let supportedTypes: [String] = [
+                kUTTypeText as String,
+                kUTTypeJSON as String,
+                kUTTypeFolder as String]
+            documentPicker = UIDocumentPickerViewController(documentTypes: supportedTypes, in: .import)
+        }
+    }
+   
+    
+    
+    
+}
+
+@available(iOS 14.0, *)
+extension OpenFileUtil : UIDocumentPickerDelegate{
+    
+    public func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentAt url: URL) {
+        
+        
+        // Start accessing a security-scoped resource.
+        guard url.startAccessingSecurityScopedResource() else {
+            self.delegate?.onError(
+                error: NSError(domain: #function, code: -1, userInfo: [
+                NSLocalizedDescriptionKey:  "enable start acessing file"
+                ]))
+            return
+        }
+        
+        do {
+            let data = try Data.init(contentsOf: url)
+            self.delegate?.onImportFile(file: data)
+        }
+        catch {
+            self.delegate?.onError(error: error)
+        }
+        
+        // Make sure you release the security-scoped resource when you finish.
+        do { url.stopAccessingSecurityScopedResource() }
+        
+    }
+    
+    public func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
+        controller.dismiss(animated: true)
+    }
+}
+
--- RS/screens/OpenFile/OpenFileViewController.swift
+++ RS/screens/OpenFile/OpenFileViewController.swift
@@ -13,84 +13,57 @@ import MobileCoreServices
 public final class OpenFileViewController: UIViewController {
     
     
+    
+    
     private let presenter: OpenFilePresenter
     
-    @IBAction func actionClick(_ sender: Any) {
-        self.openDocumentPicker()
-    }
+    private let openFileUtil: OpenFileUtil
+    
+    private let alertUtil: AlertUtil
+    
+    @IBOutlet weak var actionButton: UIButton!
+    
     
     @available(iOS, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    init(presenter: OpenFilePresenter) {
+    init(presenter: OpenFilePresenter, alertUtil: AlertUtil, openFileUtil: OpenFileUtil) {
         self.presenter = presenter
+        self.alertUtil = alertUtil
+        self.openFileUtil = openFileUtil
         
         super.init(nibName: "OpenFileViewController", bundle: Bundle.main)
     }
     
     public override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        self.actionButton.setTitle("Open file", for: .normal)
+        self.actionButton.addTarget(self,  action: #selector(buttonAction), for: .touchUpInside)
+
     }
     
-    
-    func openDocumentPicker() {
-        let documentPicker: UIDocumentPickerViewController
-        
-        if #available(iOS 14, *) {
-            // iOS 14 & later
-            let supportedTypes: [UTType] = [UTType.text, UTType.json, UTType.folder]
-            documentPicker = UIDocumentPickerViewController(forOpeningContentTypes: supportedTypes)
-        } else {
-            // iOS 13 or older code
-            let supportedTypes: [String] = [
-                kUTTypeText as String,
-                kUTTypeJSON as String,
-                kUTTypeFolder as String]
-            documentPicker = UIDocumentPickerViewController(documentTypes: supportedTypes, in: .import)
-        }
-        
-        documentPicker.delegate = self
-        documentPicker.modalPresentationStyle = .overFullScreen
-        present(documentPicker, animated: true)
+    @objc func buttonAction(sender: UIButton!) {
+        self.openFileUtil.delegate = self
+        self.openFileUtil.startPicker(view: self)
     }
+
+}
+
+extension OpenFileViewController: OpenFileUtilDelegate{
+    func onError(error: Error) {
+        let alert = self.alertUtil.getAlert(title: "Error", subtitle: "Error while reading file!")
+        present(alert, animated: true)
+    }
+    
+    func onImportFile(file: Data) {
+        let string = String(data: file, encoding: .utf8)
+        print(string)
+    }
+    
     
 }
 
 
-@available(iOS 14.0, *)
-extension OpenFileViewController : UIDocumentPickerDelegate{
-    
-    public func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentAt url: URL) {
-        
-        
-        // Start accessing a security-scoped resource.
-        guard url.startAccessingSecurityScopedResource() else {
-            // Handle the failure here.
-            return
-        }
-        
-        do {
-            let data = try Data.init(contentsOf: url)
-            // You will have data of the selected file
-            
-            let string = String(data: data, encoding: .utf8)
-            
-            print(string)
-            
-        }
-        catch {
-            print(error.localizedDescription)
-        }
-        
-        // Make sure you release the security-scoped resource when you finish.
-        do { url.stopAccessingSecurityScopedResource() }
-        
-    }
-    
-    public func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
-        self.dismiss(animated: true)
-    }
-}
